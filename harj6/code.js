215e96a73551db39ad7017c89f31e67c1cbbcdf0
Jenni Lohi <jennilohi@hotmail.com>
2021-12-08 21:23:53 +0200

    luotu persons-olio

---
+++ harj6/code.js
@@ -0,0 +1,16 @@
+
+// Luodaan persons-olio
+let persons = [
+    {
+        fname: "Reima",
+        age:25
+    },
+    {
+        fname: "Liisa",
+        age: 17
+    },
+    {
+        fname: "Sami",
+        age: 14
+    }
+]
