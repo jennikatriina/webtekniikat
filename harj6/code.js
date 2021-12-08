2b40e25242a832b766eef348ead4a0d85ab16f72
Jenni Lohi <jennilohi@hotmail.com>
2021-12-08 21:28:00 +0200

    koodia lisätty

--- harj6/code.js
+++ harj6/code.js
@@ -14,3 +14,19 @@ let persons = [
         age: 14
     }
 ]
+
+// one person = p
+let children = persons.filter( p => p.age < 18);
+
+// Lisätään ikää
+let grown = persons.map( p => {
+    p.age++;
+    return p;
+});
+
+
+let word = "s";
+
+let filteredPersons = persons.filter(p => p.fname.includes(word));
+
+let names = persons.map(p => p.fname);
