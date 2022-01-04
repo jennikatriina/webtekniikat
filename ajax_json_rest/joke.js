8d5122db1ebd692775f7a835db61a6783fd41e43
Jenni Lohi <jennilohi@hotmail.com>
2022-01-04 19:37:31 +0200

    lisätty sivulle copyright

--- ajax_json_rest/joke.js
+++ ajax_json_rest/joke.js
@@ -5,6 +5,7 @@ function get_joke_of_the_day() {
          let joke = JSON.parse(this.response);
 	     // Access the result here
 	     document.getElementById("joke").innerHTML = joke.contents.jokes[0].joke.text;
+         document.getElementById("copyright").innerHTML += joke.contents.copyright;
 
 	 }
     };
