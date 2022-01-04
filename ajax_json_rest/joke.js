9aa3383012e22690a7f823455e182eeabf77c702
Jenni Lohi <jennilohi@hotmail.com>
2022-01-04 19:33:00 +0200

    haettu olioista vitsi ja lisätty se vitsi-diviin

--- ajax_json_rest/joke.js
+++ ajax_json_rest/joke.js
@@ -2,11 +2,13 @@ function get_joke_of_the_day() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
 	 if (this.readyState == 4 && this.status == 200) {
+         let joke = JSON.parse(this.response);
 	     // Access the result here
-	     alert(this.responseText);
+	     document.getElementById("joke").innerHTML = joke.contents.jokes[0].joke.text;
+
 	 }
     };
-    xhttp.open("GET", "https://api.jokes.one/jod/categories", true);
+    xhttp.open("GET", "https://api.jokes.one/jod?category=animal", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.setRequestHeader("X-JokesOne-Api-Secret", "YOUR API HERE");
     xhttp.send();
